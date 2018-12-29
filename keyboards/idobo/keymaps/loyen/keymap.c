3f407485a7f2b3f7546f0ee860cfc7962360d918
Patrik Freij <patrik.t.freij@gmail.com>
2018-12-29 16:34:00 +0100

    Switch super and alt place

--- keyboards/idobo/keymaps/loyen/keymap.c
+++ keyboards/idobo/keymaps/loyen/keymap.c
@@ -37,7 +37,7 @@ enum custom_keycodes {
  * |------+------+------+------+------+------|------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift| Shift|  Up  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | Func | LCtrl| LSup | Alt  | Space| Space| Space| Space| Space| Space|  Alt | Ctrl | Left | Down | Right|
+ * | Func | LCtrl| Alt  | LSup | Space| Space| Space| Space| Space| Space|  Alt | Ctrl | Left | Down | Right|
  * `--------------------------------------------------------------------------------------------------------'
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RCBR, KC_NUBS, KC_DEL,  \
     KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_PIPE, KC_ENT, \
     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_ENT, \
-    FUNCTION,  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
+    FUNCTION,  KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
   [_FUNCTION] = LAYOUT_ortho_5x15( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, RESET, \
