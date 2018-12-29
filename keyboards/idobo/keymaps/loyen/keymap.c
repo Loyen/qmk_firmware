cd8227397d821e0a159ef42633eccf363a3c26b6
Patrik Freij <patrik.t.freij@gmail.com>
2018-12-29 15:26:11 +0100

    Add function layer
    Function key is found in bottom left.
    F1-F12 start from top left.
    Reset key on top right.

--- keyboards/idobo/keymaps/loyen/keymap.c
+++ keyboards/idobo/keymaps/loyen/keymap.c
@@ -17,12 +17,14 @@
 #include "action_layer.h"
 
 enum custom_layers {
-  _QWERTY
+  _QWERTY,
+  _FUNCTION
 };
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  FUNCTION,
   EXTRA
 };
 
@@ -36,17 +38,24 @@ enum custom_keycodes {
  * |------+------+------+------+------+------|------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift| Shift|  Up  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
- * | LCtrl| LSup | Alt  | Space| Space| Space| Space| Space| Space|  Alt | Ctrl | Ctrl | Left | Down | Right|
+ * | Func | LCtrl| LSup | Alt  | Space| Space| Space| Space| Space|  Alt | Ctrl | Ctrl | Left | Down | Right|
  * `--------------------------------------------------------------------------------------------------------'
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [_QWERTY] = LAYOUT_ortho_5x15( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RCBR, KC_NUBS, KC_DEL,  \
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_PIPE, KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_ENT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
+  [_QWERTY] = LAYOUT_ortho_5x15( \
+    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,  \
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RCBR, KC_NUBS, KC_DEL,  \
+    KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_PIPE, KC_ENT, \
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_ENT, \
+    FUNCTION,  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
+  [_FUNCTION] = LAYOUT_ortho_5x15( \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, RESET, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -58,6 +67,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_QWERTY);
       } else {
         // when keycode QWERTY is released
+      }
+      break;
+    case FUNCTION:
+      if (record->event.pressed) {
+        // when keycode QWERTY is pressed
+        layer_on(_FUNCTION);
+      } else {
+        // when keycode QWERTY is released
+        layer_off(_FUNCTION);
       }
       break;
     case EXTRA:
